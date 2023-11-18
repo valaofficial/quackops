c3444fa580cd2dd3a3ce2802c8ef6c5a7dc381c9
Maina Hyelavala <mhyelavala@gmail.com>
2023-11-18 02:46:29 +0100

    Adding two pistol and rifle enemy models

--- Scripts/Enemy/enemy.gd
+++ Scripts/Enemy/enemy.gd
@@ -8,7 +8,6 @@ extends CharacterBody3D
 @export var acceleration: float = 10
 @export var enemy_bullet: PackedScene
 @export var muzzle_speed = 20
-@export var time_btw_shots = 1
 
 var enemy_hp
 var speed
@@ -18,7 +17,7 @@ var is_attacking = false
 func _ready():
 	speed = set_enemy_speed()
 	call_deferred("getnav")
-	timer.wait_time = time_btw_shots
+	timer.wait_time = set_enemy_firerate()
 	enemy_hp = set_enemy_health()
 
 func _physics_process(_delta):
@@ -35,19 +34,27 @@ func _physics_process(_delta):
 	if is_in_group("ShooterEnemy"):
 		if global_position.distance_to(player_position) <= 12:
 			new_velocity = direction * Vector3(randi_range(1,2), 0, randi_range(1,2)) * speed * -1	
+			if is_in_group("PistolDuck"):
+				$AnimationPlayer.play("pistolwalkback")
 		elif  global_position.distance_to(player_position) <= 15:
 			new_velocity = Vector3.ZERO
 		
 		if global_position.distance_to(player_position) <= 20:
 			shoot()
-	elif is_in_group("MeleeEnemy"):
+		
+		if is_in_group("RifleEnemy"):
+			if velocity != Vector3.ZERO:
+				$AnimationPlayer.play("riflerun")
+			
+	if is_in_group("MeleeEnemy"):
 		if velocity != Vector3.ZERO:
 			$AnimationPlayer.play("run")
 			
 		if global_position.distance_to(player_position) < 3:
 			new_velocity = Vector3.ZERO
 			attack()
-	elif is_in_group("ClubDuck"):
+			
+	if is_in_group("ClubDuck"):
 		if velocity != Vector3.ZERO:
 			$AnimationPlayer.play("run")
 			
@@ -76,15 +83,24 @@ func die():
 	
 
 func shoot():
-	pass
-#	if can_shoot:
-#		var new_bullet = enemy_bullet.instantiate()
-#		new_bullet.speed = muzzle_speed
-#		new_bullet.global_transform = $Body/Muzzle.global_transform
-#		var scene_root = get_tree().get_root().get_children()[0]
-#		scene_root.add_child(new_bullet)
-#		can_shoot = false
-#		timer.start()
+	if can_shoot:
+		if velocity == Vector3.ZERO:
+			if is_in_group("PistolDuck"):
+				$AnimationPlayer.play("pistolstand")
+			else:
+				$AnimationPlayer.play("riflestand")
+		else:
+			if is_in_group("PistolDuck"):
+				$AnimationPlayer.play("pistolwalk")
+			else:
+				$AnimationPlayer.play("riflerun")
+		var new_bullet = enemy_bullet.instantiate()
+		new_bullet.speed = muzzle_speed
+		new_bullet.global_transform = $Body/Muzzle.global_transform
+		var scene_root = get_tree().get_root().get_children()[0]
+		scene_root.add_child(new_bullet)
+		can_shoot = false
+		timer.start()
 
 func attack():
 	is_attacking = true
@@ -96,7 +112,7 @@ func attack():
 func set_enemy_speed():
 	if is_in_group("MeleeEnemy"):
 		return 6
-	elif is_in_group("MeleeEnemy"):
+	elif is_in_group("ShooterEnemy"):
 		return 10
 	elif is_in_group("ClubDuck"):
 		return 4
@@ -104,10 +120,16 @@ func set_enemy_speed():
 func set_enemy_health():
 	if is_in_group("MeleeEnemy"):
 		return 5
-	elif is_in_group("MeleeEnemy"):
+	elif is_in_group("ShooterEnemy"):
 		return 3
 	elif is_in_group("ClubDuck"):
 		return 8
+
+func set_enemy_firerate():
+	if is_in_group("PistolDuck"):
+		return 1.35
+	elif is_in_group("RifleDuck"):
+		return .5
 
 #Signals
 func _on_navigation_agent_3d_velocity_computed(safe_velocity):
